d26431a65f5f9ae4bc4ae7f3ce39f465696885e4
Yehor Sencha <senyehor@gmail.com>
2024-07-09 17:30:05 +0300

    add printing countries data as table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from logging import Logger
 
+from make_table_to_print_from_data import make_table_to_print_from_data
 from rest_countries_scraper import RestCountriesFieldNames, RestCountriesScraper
 
 if __name__ == '__main__':
@@ -12,5 +13,6 @@ if __name__ == '__main__':
             RestCountriesFieldNames.FLAGS_AS_LINKS
         )
     )
-    result = scraper.get_data_for_chosen_fields()
-    print(result)
+    countries_data = scraper.get_data_for_chosen_fields()
+    table = make_table_to_print_from_data(['name', 'capital', 'flag_link'], countries_data)
+    print(table)
